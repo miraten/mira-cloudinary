7371c6381efe6bbb5672bb1d2a4704eebc1c33cc
Lee Sangwon <lee@miraten.com>
2014-10-22 08:53:53 +0900

    added 'remove' implementation

--- server/mira-cloudinary.js
+++ server/mira-cloudinary.js
@@ -13,12 +13,21 @@ var Future = Npm.require('fibers/future');
 
 Meteor.methods({
   'cloudinaryUpload': function(file, options) {
-
     var future = new Future();
 
     cloudinary.uploader.upload(file, function(result) {
       future.return(result);
     }, options);
+
+    return future.wait();
+  },
+  
+  'cloudinaryRemove': function(public_id) {
+    var future = new Future();
+
+    cloudinary.uploader.destroy(file, function(result) {
+      future.return(result);
+    });
 
     return future.wait();
   }
